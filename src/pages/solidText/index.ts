81bd987c44f1c122a4930a187a150e0cd3b8bfd2
jiwon79 <19-079@ksa.hs.kr>
2023-03-07 01:56:37 +0900

    :sparkles: solid text template page 추가

---
+++ src/pages/solidText/index.ts
@@ -0,0 +1,13 @@
+import Component from "../../model/component";
+
+class SolidTextPage extends Component {
+  template(): string {
+    return `
+      <div>
+        solid text
+      </div>
+    `;
+  }
+}
+
+export default SolidTextPage;
