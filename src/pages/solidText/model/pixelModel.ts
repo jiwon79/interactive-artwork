8a6030cc9fc85cdb0f5e9f65dcf36e5601dfab43
jiwon79 <19-079@ksa.hs.kr>
2023-08-30 02:22:30 +0900

    :card_file_box: PixelModel 추가

---
+++ src/pages/solidText/model/pixelModel.ts
@@ -0,0 +1,19 @@
+import Vector from "@utils/vector";
+import { Parameter } from "../utils/type";
+
+interface PixelModel {
+  parameter: Parameter;
+  r: Vector;
+  normal: Vector;
+}
+
+export const emptyPixelModel: PixelModel = {
+  parameter: {
+    theta: 0,
+    phi: 0
+  },
+  r: new Vector([0, 0, 0]),
+  normal: new Vector([0, 0, 0])
+}
+
+export default PixelModel;
