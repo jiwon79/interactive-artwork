eda0c9cc0b7d1ddc279a96bd9c174d5dd302c74a
jiwon79 <19-079@ksa.hs.kr>
2023-09-11 11:31:56 +0900

    :sparkles: Crowd/BezierService 추가

---
+++ src/pages/crowd/service/bezierService.ts
@@ -0,0 +1,34 @@
+import Vector from "@utils/vector";
+
+class BezierService {
+  public getCurvePoint(initPoints: [Vector, Vector, Vector], t: number): Vector {
+    const [a, b, c] = initPoints;
+    let aVector = a.multiple((1-t) * (1-t));
+    let bVector = b.multiple(2 * t * (1-t));
+    let cVector = c.multiple(t * t);
+
+    return aVector.add(bVector).add(cVector);
+  }
+
+  public getCurveSlope(initPoints: [Vector, Vector, Vector], t: number): Vector {
+    const [a, b, c] = initPoints;
+    let aVector = a.multiple(2 * (1-t));
+    let bVector = b.multiple(2 * (1-2 * t));
+    let cVector = c.multiple(2 * t);
+
+    return aVector.add(bVector).add(cVector);
+  }
+
+  public getCurve(initPoints: [Vector, Vector, Vector]): Vector[] {
+    const curve: Vector[] = [];
+
+    for (let t = 0; t < 1; t += 0.01) {
+      curve.push(this.getCurvePoint(initPoints, t))
+    }
+
+    return curve;
+  }
+}
+
+export default BezierService
+
